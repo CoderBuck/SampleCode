bfa7ad32689998afc361ff244b445bee922a7fc5
buck <coderbuck@163.com>
2018-10-31 10:05:45 +0800

    0

--- app/src/main/java/me/buck/samplecode/tool/CountDownTimer.java
+++ app/src/main/java/me/buck/samplecode/tool/CountDownTimer.java
@@ -7,120 +7,121 @@ import android.os.SystemClock;
 
 /**
  * 请使用 SimpleCountDownTimer
- *
+ * <p>
  * 这个是从API28源码里拷贝的，8.0 之前的版本有问题，会跳过最后一次的 onTick
  * 链接：https://android.googlesource.com/platform/frameworks/base/+/0c3149191fbbfdc5242f8dd8a02f561f1ce604c3%5E%21/#F0
  */
 abstract class CountDownTimer {
 
-        /**
-         * Millis since epoch when alarm should stop.
-         */
-        private final long mMillisInFuture;
+    /**
+     * Millis since epoch when alarm should stop.
+     */
+    private final long mMillisInFuture;
 
-        /**
-         * The interval in millis that the user receives callbacks
-         */
-        private final long mCountdownInterval;
+    /**
+     * The interval in millis that the user receives callbacks
+     */
+    private final long mCountdownInterval;
 
-        private long mStopTimeInFuture;
+    private long mStopTimeInFuture;
 
-        /**
-         * boolean representing if the timer was cancelled
-         */
-        private boolean mCancelled = false;
+    /**
+     * boolean representing if the timer was cancelled
+     */
+    private boolean mCancelled = false;
 
-        /**
-         * @param millisInFuture The number of millis in the future from the call
-         *   to {@link #start()} until the countdown is done and {@link #onFinish()}
-         *   is called.
-         * @param countDownInterval The interval along the way to receive
-         *   {@link #onTick(long)} callbacks.
-         */
-        public CountDownTimer(long millisInFuture, long countDownInterval) {
-            mMillisInFuture = millisInFuture;
-            mCountdownInterval = countDownInterval;
-        }
+    /**
+     * @param millisInFuture    The number of millis in the future from the call
+     *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+     *                          is called.
+     * @param countDownInterval The interval along the way to receive
+     *                          {@link #onTick(long)} callbacks.
+     */
+    public CountDownTimer(long millisInFuture, long countDownInterval) {
+        mMillisInFuture = millisInFuture;
+        mCountdownInterval = countDownInterval;
+    }
 
-        /**
-         * Cancel the countdown.
-         */
-        public synchronized final void cancel() {
-            mCancelled = true;
-            mHandler.removeMessages(MSG);
-        }
+    /**
+     * Cancel the countdown.
+     */
+    public synchronized final void cancel() {
+        mCancelled = true;
+        mHandler.removeMessages(MSG);
+    }
 
-        /**
-         * Start the countdown.
-         */
-        public synchronized final CountDownTimer start() {
-            mCancelled = false;
-            if (mMillisInFuture <= 0) {
-                onFinish();
-                return this;
-            }
-            mStopTimeInFuture = SystemClock.elapsedRealtime() + mMillisInFuture;
-            mHandler.sendMessage(mHandler.obtainMessage(MSG));
+    /**
+     * Start the countdown.
+     */
+    public synchronized final CountDownTimer start() {
+        mCancelled = false;
+        if (mMillisInFuture <= 0) {
+            onFinish();
             return this;
         }
+        mStopTimeInFuture = SystemClock.elapsedRealtime() + mMillisInFuture;
+        mHandler.sendMessage(mHandler.obtainMessage(MSG));
+        return this;
+    }
 
 
-        /**
-         * Callback fired on regular interval.
-         * @param millisUntilFinished The amount of time until finished.
-         */
-        public abstract void onTick(long millisUntilFinished);
+    /**
+     * Callback fired on regular interval.
+     *
+     * @param millisUntilFinished The amount of time until finished.
+     */
+    public abstract void onTick(long millisUntilFinished);
 
-        /**
-         * Callback fired when the time is up.
-         */
-        public abstract void onFinish();
-
-
-        private static final int MSG = 1;
+    /**
+     * Callback fired when the time is up.
+     */
+    public abstract void onFinish();
 
 
-        // handles counting down
-        @SuppressLint("HandlerLeak") private Handler mHandler = new Handler() {
+    private static final int MSG = 1;
 
-            @Override
-            public void handleMessage(Message msg) {
 
-                synchronized (CountDownTimer.this) {
-                    if (mCancelled) {
-                        return;
-                    }
+    // handles counting down
+    @SuppressLint("HandlerLeak") private Handler mHandler = new Handler() {
 
-                    final long millisLeft = mStopTimeInFuture - SystemClock.elapsedRealtime();
+        @Override
+        public void handleMessage(Message msg) {
 
-                    if (millisLeft <= 0) {
-                        onFinish();
+            synchronized (CountDownTimer.this) {
+                if (mCancelled) {
+                    return;
+                }
+
+                final long millisLeft = mStopTimeInFuture - SystemClock.elapsedRealtime();
+
+                if (millisLeft <= 0) {
+                    onFinish();
+                } else {
+                    long lastTickStart = SystemClock.elapsedRealtime();
+                    onTick(millisLeft);
+
+                    // take into account user's onTick taking time to execute
+                    long lastTickDuration = SystemClock.elapsedRealtime() - lastTickStart;
+                    long delay;
+
+                    if (millisLeft < mCountdownInterval) {
+                        // just delay until done
+                        delay = millisLeft - lastTickDuration;
+
+                        // special case: user's onTick took more than interval to
+                        // complete, trigger onFinish without delay
+                        if (delay < 0) delay = 0;
                     } else {
-                        long lastTickStart = SystemClock.elapsedRealtime();
-                        onTick(millisLeft);
+                        delay = mCountdownInterval - lastTickDuration;
 
-                        // take into account user's onTick taking time to execute
-                        long lastTickDuration = SystemClock.elapsedRealtime() - lastTickStart;
-                        long delay;
-
-                        if (millisLeft < mCountdownInterval) {
-                            // just delay until done
-                            delay = millisLeft - lastTickDuration;
-
-                            // special case: user's onTick took more than interval to
-                            // complete, trigger onFinish without delay
-                            if (delay < 0) delay = 0;
-                        } else {
-                            delay = mCountdownInterval - lastTickDuration;
-
-                            // special case: user's onTick took more than interval to
-                            // complete, skip to next interval
-                            while (delay < 0) delay += mCountdownInterval;
-                        }
-
-                        sendMessageDelayed(obtainMessage(MSG), delay);
+                        // special case: user's onTick took more than interval to
+                        // complete, skip to next interval
+                        while (delay < 0) delay += mCountdownInterval;
                     }
+
+                    sendMessageDelayed(obtainMessage(MSG), delay);
                 }
             }
-        };
-    }
+        }
+    };
+}
