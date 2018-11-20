fc338ff4b1994e1489d734782ad945a9aa376bb6
buck <coderbuck@163.com>
2018-11-20 16:52:10 +0800

    整理

--- app/src/main/java/me/buck/samplecode/MainActivity.java
+++ app/src/main/java/me/buck/samplecode/MainActivity.java
@@ -1,13 +1,7 @@
 package me.buck.samplecode;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import me.buck.samplecode.test.SeekBarActivity;
-import me.buck.samplecode.tool.SimpleCountDownTimer;
-import me.buck.samplecode.tool.TimingLogger;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity==";
@@ -16,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String s = getResources().getDisplayMetrics().toString();
-        Log.e(TAG, "onCreate: getDisplayMetrics = " + s );
 
     }
 
