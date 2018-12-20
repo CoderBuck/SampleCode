c5469cbdbaae0f5028d4a8a99f8af78935ff5983
buck <coderbuck@163.com>
2018-12-20 17:18:24 +0800

    catch 广播未注册异常

--- app/src/main/java/me/buck/samplecode/tool/SimpleReceiver.java
+++ app/src/main/java/me/buck/samplecode/tool/SimpleReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 /**
  * 广播接收器的简单封装，默认接收本地广播
@@ -33,10 +34,16 @@ public abstract class SimpleReceiver extends BroadcastReceiver {
     }
 
     public void unregister() {
-        if (mIsLocal) {
-            LocalBroadcastManager.getInstance(mContext).unregisterReceiver(this);
-        } else {
-            mContext.unregisterReceiver(this);
+        try {
+            if (mIsLocal) {
+                LocalBroadcastManager.getInstance(mContext).unregisterReceiver(this);
+            } else {
+                mContext.unregisterReceiver(this);
+            }
+        } catch (IllegalArgumentException e) {
+            // 有时候会抛出没有注册的异常，在 activity 的 onCreate 和 onDestroy 注册和取消注册时，快速创建销毁activity会出现此异常
+            e.printStackTrace();
+            Log.e("SimpleReceiver", "unregister: " + e.getMessage());
         }
     }
 }
